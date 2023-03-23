7a7bed806dc8fcb3f41a0bcc6344580ae8afd62f
Cyrill Leutwiler <bigcyrill@hotmail.com>
2023-03-23 18:28:09 +0100

    fix networking
    Signed-off-by: Cyrill Leutwiler <bigcyrill@hotmail.com>

--- microvm.sh
+++ microvm.sh
@@ -12,6 +12,7 @@ DATA_DIR=$1
 mkdir -p $BASE_DIR
 cp $DATA_DIR/rootfs$3.img $BASE_DIR/rootfs.img
 cp $DATA_DIR/config.json $DATA_DIR/vmlinux.bin $BASE_DIR/
+sed -i "s/00:02/0${3}:02/g" $BASE_DIR/config.json
 sed -i "s/tap0/tap${3}/g" $BASE_DIR/config.json
 chown -R jailer:jailer $BASE_DIR
 
