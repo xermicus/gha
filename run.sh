76b9e3559abeb486cbf72b7e98884770938d4239
Cyrill Leutwiler <bigcyrill@hotmail.com>
2023-03-23 17:32:30 +0100

    more bugfix
    Signed-off-by: Cyrill Leutwiler <bigcyrill@hotmail.com>

--- run.sh
+++ run.sh
@@ -6,14 +6,12 @@ then
 	echo "Usage: run.sh UPLINK_DEV DATA_DIR BASE_DIR VM_NO"
 	exit 1
 fi
-BASE_DIR=$3/$4/root
 
 # Network setup is idempotent
 
 TAP_DEV="tap0"
 TAP_IP="172.16.0.1"
 MASK_SHORT="/28"
-BASE_DIR=/srv/jailer/firecracker
 
 # Setup network interface
 ip link del "$TAP_DEV" 2> /dev/null || true
@@ -33,10 +31,10 @@ iptables -I FORWARD 1 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -I FORWARD 1 -i tap0 -o $1 -j ACCEPT
 
 # Clean state
-rm -rf $BASE_DIR
+rm -rf $3
 
 for n in $(seq 1 $4);
 do
-	$3/microvm.sh $2 $BASE_DIR $n &
+	$3/microvm.sh $2 $3 $n &
 done
 
